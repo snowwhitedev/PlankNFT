cd8756c2719548dad4fc32b4309f5a89eb812795
snowwhitedev <snowwhite91924@gmail.com>
2021-08-22 03:05:12 +0700

    feat: changed planks to mapping and fix bugs

--- contracts/PolyPlankToken.sol
+++ contracts/PolyPlankToken.sol
@@ -7,6 +7,8 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+// import "hardhat/console.sol";
+
 contract PolyPlankToken is ERC721URIStorage, Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
 
@@ -22,9 +24,8 @@ contract PolyPlankToken is ERC721URIStorage, Ownable, ReentrancyGuard {
     uint256 private _buyingPrice = 25 ether;
     address payable private _devWallet;
 
-    uint256 public SalesRemaining;
-
-    uint256[] public AvailablePlanks;
+    uint256 public salesRemaining;
+    mapping(uint256 => uint256) public availablePlanks;
 
     mapping(address => Promoter) private promoterClaims;
     mapping(address => uint256) private nonce;
@@ -39,11 +40,9 @@ contract PolyPlankToken is ERC721URIStorage, Ownable, ReentrancyGuard {
     }
 
     function removeAvailablePlank(uint256 indexToRemove) internal {
-        SalesRemaining--;
-        for (uint256 i = indexToRemove; i < AvailablePlanks.length - 1; i++) {
-            AvailablePlanks[i] = AvailablePlanks[i + 1];
-        }
-        AvailablePlanks.pop();
+        salesRemaining--;
+        availablePlanks[indexToRemove] = availablePlanks[salesRemaining];
+        availablePlanks[salesRemaining] = 0;
     }
 
     /**
@@ -51,9 +50,8 @@ contract PolyPlankToken is ERC721URIStorage, Ownable, ReentrancyGuard {
      */
     function randomIndex() internal returns (uint256) {
         nonce[msg.sender]++;
-        uint256 index = uint256(keccak256(abi.encodePacked(nonce[msg.sender], msg.sender, block.difficulty))) %
-            AvailablePlanks.length;
-        return index + 1;
+        uint256 index = uint256(keccak256(abi.encodePacked(nonce[msg.sender], msg.sender, block.difficulty))) % salesRemaining;
+        return index;
     }
 
     function mintPlank(string memory tokenURI) external onlyOwner returns (uint256) {
@@ -64,8 +62,9 @@ contract PolyPlankToken is ERC721URIStorage, Ownable, ReentrancyGuard {
         uint256 newItemId = _tokenIds.current();
         _mint(address(this), newItemId);
         _setTokenURI(newItemId, tokenURI);
-        AvailablePlanks.push(newItemId);
-        SalesRemaining++;
+        availablePlanks[salesRemaining] = newItemId;
+
+        salesRemaining++;
 
         return newItemId;
     }
@@ -75,25 +74,27 @@ contract PolyPlankToken is ERC721URIStorage, Ownable, ReentrancyGuard {
     }
 
     function claimPromoter() external nonReentrant onlyPromoter {
-        require(1 <= SalesRemaining, "There aren't enough left to buy that many");
+        require(1 <= salesRemaining, "There aren't enough left to buy that many");
         require(!promoterClaims[msg.sender].isClaimed, "You have already claimed your Plank");
         uint256 indexOfSale = randomIndex();
+        uint256 tokenId = availablePlanks[indexOfSale];
         removeAvailablePlank(indexOfSale);
         promoterClaims[msg.sender].isClaimed = true;
-        _safeTransfer(address(this), msg.sender, indexOfSale, "");
+        _safeTransfer(address(this), msg.sender, tokenId, "");
     }
 
     function buyPlanks(uint256 quantity) external payable nonReentrant {
         require(quantity > 0, "Must buy at least one");
         require(msg.value == quantity * _buyingPrice, "Incorrect amount of matic sent");
-        require(quantity <= SalesRemaining, "There aren't enough left to buy that many");
+        require(quantity <= salesRemaining, "There aren't enough left to buy that many");
 
         _devWallet.transfer(msg.value);
 
         for (uint256 index = 0; index < quantity; index++) {
             uint256 indexOfSale = randomIndex();
+            uint256 tokenId = availablePlanks[indexOfSale];
             removeAvailablePlank(indexOfSale);
-            _safeTransfer(address(this), msg.sender, indexOfSale, "");
+            _safeTransfer(address(this), msg.sender, tokenId, "");
         }
     }
 }
